bc44cd6befa58bc5c1bb396bf00bd14977e2768e
Yuta Yasugahira <hourou_freak@yahoo.co.jp>
2018-05-15 07:16:50 +0900

    Fix bug

--- src/main/java/com/spring/blog/web/LoginController.java
+++ src/main/java/com/spring/blog/web/LoginController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
-	@GetMapping
+	@GetMapping(path = "/")
 	String top() {
 		return "redirect:/articles";
 	}
