8f8050f209569bd9fe12c79f254aab2b9bd0d5c4
yasugahira0810 <hourou_freak@yahoo.co.jp>
2019-04-30 13:35:38 +0900

    Add a test case

--- src/test/java/com/spring/blog/SpringBlogApplicationTests.java
+++ src/test/java/com/spring/blog/SpringBlogApplicationTests.java
@@ -2,15 +2,30 @@ package com.spring.blog;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Assertions.*;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SpringBlogApplicationTests {
+	@LocalServerPort
+	int port;
+	@Autowired
+	TestRestTemplate restTemplate;
 
 	@Test
 	public void contextLoads() {
+		ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:" + port, String.class);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(response.getBody()).contains("退屈の花");
 	}
 
 }
